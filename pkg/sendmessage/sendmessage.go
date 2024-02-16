180ea81a5232552ab2f9e44a0a5f7ce690df6b11
Sergio Luis <sergio.javax@gmail.com>
2024-02-15 20:21:56 -0500

    sendmessage: changes for v1.0.3

--- pkg/sendmessage/sendmessage.go
+++ pkg/sendmessage/sendmessage.go
@@ -2,7 +2,7 @@ package send
 
 import "fmt"
 
-func SendMessage(message string) {
+func sendMessage(message string) {
 	fmt.Println(message)
 	fmt.Println("Hemos actualizado nuestras politicas de seguridad")
 }
